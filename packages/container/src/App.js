982fd5d8037bdb657797f179a4aaa1ebb4074184
gourabzensar <gourab.konar@zensar.com>
2024-09-02 23:20:35 +0530

    added container random class

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import MarketingApp from "./components/marketingApp";
 import Header from "./components/Header";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 export default function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 }
