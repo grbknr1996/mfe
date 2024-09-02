6732626d2733c9523dff36c85056ca6fdbc34ea1
gourabzensar <gourab.konar@zensar.com>
2024-09-02 18:16:42 +0530

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import MarketingApp from "./components/marketingApp";
+import Header from "./components/Header";
 export default function App() {
   return (
-    <div>
-      <h1>Container App!!!!!!</h1>
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 }
